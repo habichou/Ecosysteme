0c8709646c21e7d492a810de71a1850f69b5a323
habichou <houda.abichou@telecom-bretagne.eu>
2019-12-18 19:30:56 +0100

    add convenient CImg.h file

---
+++ IBestiole.cpp
@@ -0,0 +1,5 @@
+//
+// Created by houda on 18/12/2019.
+//
+
+#include "IBestiole.h"
---
+++ IBestiole.h
@@ -0,0 +1,16 @@
+//
+// Created by houda on 18/12/2019.
+//
+
+#ifndef ECOSYSTEME_IBESTIOLE_H
+#define ECOSYSTEME_IBESTIOLE_H
+
+
+
+class IBestiole {
+
+};
+
+
+
+#endif //ECOSYSTEME_IBESTIOLE_H
